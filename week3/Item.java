3ce9c186233143d16a283c8c07a1973167941726
BrixManzanero <111488241+BrixManzanero@users.noreply.github.com>
2022-09-01 15:39:49 +0800

    Add files via upload

---
+++ week3/Item.java
@@ -0,0 +1,12 @@
+public class Item {
+    int price;
+    int weight;
+    int quantatity;
+    String description; 
+    boolean canbetraded; 
+
+    public void getDescription() {
+        System.out.println(description);
+
+    } 
+}
---
+++ week3/Pet.java
@@ -0,0 +1,24 @@
+public class Pet{
+    String name;
+    int trustlevel;
+    int petlevel;
+    boolean ishappy;
+    
+    public void myPet(){ 
+        System.out.println("This is my pet" + name + " and he/she is Happy");
+
+       
+    }
+    public Pet(String nam, int trustlev, int petlev, boolean ishap){
+        name = nam;
+        trustlevel = trustlev;
+        petlevel = petlev;
+        ishappy = ishap;
+
+    }
+
+}
+
+
+
+
